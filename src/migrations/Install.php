6863ef71f5f9219c0248138f2f61bc410128678c
Caroline Boeykens <caroline@statik.be>
2019-09-06 13:07:27 +0200

    install migration

--- src/migrations/Install.php
+++ src/migrations/Install.php
@@ -33,133 +33,41 @@ use statikbe\export\records\ExportRecord;
  */
 class Install extends Migration
 {
-    // Public Properties
-    // =========================================================================
 
-    /**
-     * @var string The database driver to use
-     */
-    public $driver;
-
-    // Public Methods
-    // =========================================================================
-
-    /**
-     * This method contains the logic to be executed when applying this migration.
-     * This method differs from [[up()]] in that the DB logic implemented here will
-     * be enclosed within a DB transaction.
-     * Child classes may implement this method instead of [[up()]] if the DB logic
-     * needs to be within a transaction.
-     *
-     * @return boolean return a false value to indicate the migration fails
-     * and should not proceed further. All other return values mean the migration succeeds.
-     */
     public function safeUp()
     {
-        $this->driver = Craft::$app->getConfig()->getDb()->driver;
-        if ($this->createTables()) {
-            //$this->createIndexes();
-            $this->addForeignKeys();
-            // Refresh the db schema caches
-            Craft::$app->db->schema->refresh();
-            $this->insertDefaultData();
-        }
+        $this->createTables();
 
         return true;
     }
 
-    /**
-     * This method contains the logic to be executed when removing this migration.
-     * This method differs from [[down()]] in that the DB logic implemented here will
-     * be enclosed within a DB transaction.
-     * Child classes may implement this method instead of [[down()]] if the DB logic
-     * needs to be within a transaction.
-     *
-     * @return boolean return a false value to indicate the migration fails
-     * and should not proceed further. All other return values mean the migration succeeds.
-     */
     public function safeDown()
     {
-        $this->driver = Craft::$app->getConfig()->getDb()->driver;
         $this->removeTables();
 
         return true;
     }
 
-    // Protected Methods
-    // =========================================================================
-
-    /**
-     * Creates the tables needed for the Records used by the plugin
-     *
-     * @return bool
-     */
     protected function createTables()
     {
-        $tablesCreated = false;
-
-    // export_exportrecord table
-        $tableSchema = Craft::$app->db->schema->getTableSchema(ExportRecord::tableName());
-        if ($tableSchema === null) {
-            $tablesCreated = true;
-            $this->createTable(
-                ExportRecord::tableName(),
-                [
-                    'id' => $this->primaryKey(),
-                    'name' => $this->string(255)->notNull()->defaultValue(''),
-                    'filename' => $this->string(255)->notNull()->defaultValue(''),
-                    'exportType' => $this->string()->notNull()->defaultValue(''),
-                    'elementType' => $this->string()->notNull()->defaultValue(''),
-                    'elementGroup' => $this->string()->notNull()->defaultValue(''),
-                    'siteId' => $this->integer()->notNull()->defaultValue('1'),
-                    'sortOrder' => $this->smallInteger()->unsigned(),
-                    'settings' => $this->string(10000)->defaultValue(''),
-                    'dateCreated' => $this->dateTime()->notNull(),
-                    'dateUpdated' => $this->dateTime()->notNull(),
-                    'uid' => $this->uid(),
-                ]
-            );
-        }
-
-        return $tablesCreated;
+        $this->createTable(ExportRecord::tableName(), [
+            'id' => $this->primaryKey(),
+            'name' => $this->string(255)->notNull()->defaultValue(''),
+            'filename' => $this->string(255)->notNull()->defaultValue(''),
+            'exportType' => $this->string()->notNull()->defaultValue(''),
+            'elementType' => $this->string()->notNull()->defaultValue(''),
+            'elementGroup' => $this->string()->notNull()->defaultValue(''),
+            'siteId' => $this->integer()->notNull()->defaultValue('1'),
+            'sortOrder' => $this->smallInteger()->unsigned(),
+            'settings' => $this->string(10000)->defaultValue(''),
+            'dateCreated' => $this->dateTime()->notNull(),
+            'dateUpdated' => $this->dateTime()->notNull(),
+            'uid' => $this->uid(),
+        ]);
     }
 
-    /**
-     * Creates the foreign keys needed for the Records used by the plugin
-     *
-     * @return void
-     */
-    protected function addForeignKeys()
-    {
-    // export_exportrecord table
-        $this->addForeignKey(
-            $this->db->getForeignKeyName(ExportRecord::tableName(), 'siteId'),
-            ExportRecord::tableName(),
-            'siteId',
-            '{{%sites}}',
-            'id',
-            'CASCADE',
-            'CASCADE'
-        );
-    }
-
-    /**
-     * Populates the DB with the default data.
-     *
-     * @return void
-     */
-    protected function insertDefaultData()
-    {
-    }
-
-    /**
-     * Removes the tables needed for the Records used by the plugin
-     *
-     * @return void
-     */
     protected function removeTables()
     {
-    // export_exportrecord table
         $this->dropTableIfExists(ExportRecord::tableName());
     }
 }
